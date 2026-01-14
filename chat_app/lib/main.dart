0e5ee8e669c4f9a730f700f6304273d57606e4d1
ZabihullahNooriWardak <zabinoori712@gmail.com>
2026-01-14 07:10:07 +0430

    config theme to the app

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -12,7 +12,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        // primaryColor: Color(0xFF075E54),
+        primaryColor: Color(0xFF075E54),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Color(0xFF075E54),
+          foregroundColor: Colors.white,
+        ),
+        tabBarTheme: TabBarThemeData(
+          labelColor: Colors.white,
+          unselectedLabelColor: Colors.white70,
+          indicator: UnderlineTabIndicator(
+            borderSide: BorderSide(color: Colors.white, width: 2.0),
+          ),
+        ),
         // scaffoldBackgroundColor: Color(0xFFECE5DD),
         //  appBarTheme: AppBarTheme(
         //     backgroundColor: Color(0xFF075E54),
