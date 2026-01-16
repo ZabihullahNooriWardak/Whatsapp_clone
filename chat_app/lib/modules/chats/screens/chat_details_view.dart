7f762db6f94badaa894af075aefaf3fa893be536
ZabihullahNooriWardak <zabinoori712@gmail.com>
2026-01-16 21:16:47 +0430

    Designed the message sending part

--- chat_app/lib/modules/chats/screens/chat_details_view.dart
+++ chat_app/lib/modules/chats/screens/chat_details_view.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 class ChatDetailsView extends StatefulWidget {
   const ChatDetailsView({super.key, required this.chatModel});
   final ChatModel chatModel;
+
   @override
   State<ChatDetailsView> createState() => _ChatDetailsViewState();
 }
@@ -12,6 +13,7 @@ class _ChatDetailsViewState extends State<ChatDetailsView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blueGrey,
       appBar: AppBar(
         leadingWidth: double.infinity,
         leading: InkWell(
@@ -34,7 +36,10 @@ class _ChatDetailsViewState extends State<ChatDetailsView> {
                     widget.chatModel.name,
                     style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
                   ),
-                  Text('last seen today at 12:00 AM', style: TextStyle(fontSize: 10)),
+                  Text(
+                    'last seen today at 12:00 AM',
+                    style: TextStyle(fontSize: 10),
+                  ),
                 ],
               ),
             ],
@@ -50,6 +55,61 @@ class _ChatDetailsViewState extends State<ChatDetailsView> {
                 PopupMenuItem(child: Text('Mute notification')),
               ];
             },
+          ),
+        ],
+      ),
+      body: Stack(
+        children: [
+          ListView(), // Your chat messages
+          SafeArea(
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4),
+                child: Row(
+                  children: [
+                    // Input field
+                    Expanded(
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                        margin: EdgeInsets.zero,
+                        child: TextFormField(
+                          textAlignVertical: TextAlignVertical.center,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Type a message',
+                            contentPadding: EdgeInsets.symmetric(vertical: 5),
+                            prefixIcon: Icon(Icons.emoji_emotions),
+                        
+                            suffixIcon: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: Icon(Icons.attach_file),
+                                ),
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: Icon(Icons.camera_alt),
+                                ),
+                              ],
+                            ),
+                      
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 8),
+                    CircleAvatar(
+                      radius: 25,
+                      child: Icon(Icons.mic),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
         ],
       ),
